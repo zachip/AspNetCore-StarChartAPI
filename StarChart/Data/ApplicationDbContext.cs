f9a6e3534ad20d4d30e2258de1cc714fa3645ccb
Zach Hipsher <HipsherZ@bv.com>
2019-07-19 11:20:35 -0500

    Fixed DbSet... For real this time,....

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,10 +5,10 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<CelestialObject> CelestialObjects;
-
     }
 }
